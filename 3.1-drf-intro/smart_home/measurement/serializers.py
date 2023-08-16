532e87194069323021acd6922ec9a6e92f8bcd44
АриоХ <123810575+IlyaDyakonov@users.noreply.github.com>
2023-08-16 20:39:46 +0400

    Update serializers.py

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -8,6 +8,10 @@ class MeasurementSerializer(serializers.ModelSerializer):
         model = Measurement
         fields = ['id', 'sensor', 'temperature', 'created_at']
 
+class MeasurementSerializerCreate(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['id', 'sensor', 'temperature', 'created_at', 'sensor_id']
 
 class SensorDetailSerializer(serializers.ModelSerializer):
     measurements = MeasurementSerializer(read_only=True, many=True)
